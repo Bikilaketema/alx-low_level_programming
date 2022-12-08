a2096fea4ef4dccff49839dd433f78619840641c
bikilaketema <bikilaketema94@gmail.com>
2022-12-08 15:16:12 +0300

    done with task 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef LISTS_H //lists.h
-#define LISTS_H //lists.h
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -12,14 +12,12 @@
  *
  * Description: doubly linked list node structure
  */
-
 typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -31,4 +29,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif // lists.h
+#endif /* LISTS_H */
